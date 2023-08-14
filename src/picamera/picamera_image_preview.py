c07e563c37bb7d0579dc170b06e5a81c0fbb48d5
AUAFRPI4 <alvaroachury@gmail.com>
2023-08-14 13:57:57 -0500

    Adjust see-through level

--- src/picamera/picamera_image_preview.py
+++ src/picamera/picamera_image_preview.py
@@ -12,7 +12,8 @@ camera = PiCamera()
 camera.start_preview()
 
 camera.rotation = 180
-camera.start_preview()
+
+camera.start_preview(alpha=200) # apha value adjust the see-though setting level from 0 to 255
 sleep(5)
 camera.stop_preview()
 
